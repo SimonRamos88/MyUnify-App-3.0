edd5739e601497846788058ea2e59eb5afd91ade
SimonRamos88 <joramos@unal.edu.co>
2023-03-05 20:17:18 -0500

    Creacion clase lugar en la carpeta features-eventos-domian-entities

---
+++ lib/features/eventos/domain/entities/lugar.dart
@@ -0,0 +1,25 @@
+
+class Lugar{
+
+  int idLugar;
+  String direccion;
+  String nombre;
+  int aforo;
+  int aireLibre;
+  int cover;
+
+  // CONSTRUCTOR
+  Lugar({
+    required this.idLugar,
+    required this.direccion,
+    required this.nombre,
+    required this.aforo,
+    required this.aireLibre,
+    required this.cover
+  });
+
+  // METODOS
+
+  //ConsultarLLegada(){
+  //}
+}
